832501254b03abecfe24c32ef57ab51f8d2c3882
Melkov Denis <d.a.melkov@gmail.com>
2020-08-08 01:27:26 +0300

    add 2 view: simple, engineer; add button to change view

--- app/src/main/java/com/astra/melkovhw72/MainActivity.java
+++ app/src/main/java/com/astra/melkovhw72/MainActivity.java
@@ -3,6 +3,9 @@ package com.astra.melkovhw72;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final View simple = findViewById(R.id.simple_view);
+        final View engineer = findViewById(R.id.engineer_view);
+
+        Button btnChangeView = findViewById(R.id.button_change_view);
+        btnChangeView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(simple.getVisibility() == View.GONE) {
+                       simple.setVisibility(View.VISIBLE);
+                       engineer.setVisibility(View.GONE);
+                } else {
+                    simple.setVisibility(View.GONE);
+                    engineer.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
